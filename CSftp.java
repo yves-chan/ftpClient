3c57c4c51f23d5a6c70eb6815d414b99dddf7464
yves-chan <chanyves@gmail.com>
2016-10-07 12:18:25 -0700

    set up most conditions

--- CSftp.java
+++ CSftp.java
@@ -51,83 +51,128 @@ public class CSftp {
                 // To send data to socket
                 stdIn = new BufferedReader(new InputStreamReader(System.in));
 
-                String userInput;
-                while ((userInput = stdIn.readLine()) != null) {
-                    out.println(userInput);
-                    System.out.println("echo: " + reader.readLine());
+                String userName;
+                String password;
+                //TODO: Need a check for logged into FTP before doing other commands
+
+                for (int len = 1; len > 0; ) {
+                    System.out.print("csftp> ");
+                    //len = System.in.read(cmdString);
+                    String userInput;
+                    String[] userInputArray;
+                    if ((userInput = stdIn.readLine()) != null) {
+                        userInputArray = userInput.split(" ");
+                        switch (userInputArray[0].toLowerCase()) {
+                            case "user" :
+                                // Login as user, pass user input array for args
+                                logIn(userInputArray, out, reader);
+                                break;
+
+                            case "pw" :
+                                enterPassword(userInputArray, out, reader);
+                                break;
+
+                            case "quit" :
+                                quit(out, reader);
+                                break;
+
+                            case "get" :
+                                getRemote(userInputArray, out, reader);
+                                break;
+
+                            case "cd" :
+                                cdDirectory(userInputArray, out, reader);
+                                break;
+
+                            case "dir" :
+                                showDir(out, reader);
+                                break;
+                            default:
+                                // Start processing the command here.
+                                System.out.println("900 Invalid command.");
+                        }
+
+                    }
+
+
+                    if (len <= 0)
+                        break;
+
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
+            } catch (IOException exception) {
+                System.err.println("998 Input error while reading commands, terminating.");
             }
 
         } else {
             System.out.print("First Argument must be IP or DNS\n");
             return;
         }
+    }
 
+    /* TODO:
+Establishes a data connection and retrieves a list of files in the current working directory
+on the server. The list is printed to standard output.
 
+FTP command: PASV, LIST
+Application command: dir
+*/
+    private static void showDir(PrintWriter out, BufferedReader reader) {
+    }
+
+    /* TODO:
+Changes the current working directory on the server to the directory indicated by DIRECTORY.
+
+FTP command: CWD
+Application Command: cd DIRECTORY
+*/
+    private static void cdDirectory(String[] userInputArray, PrintWriter out, BufferedReader reader) {
+    }
+
+    /* TODO:
+Establishes a data connection and retrieves the file indicated by REMOTE,
+saving it in a file of the same name on the local machine.
+
+FTP command: PASV, RETR
+Application command: get REMOTE
+*/
+    private static void getRemote(String[] userInputArray, PrintWriter out, BufferedReader reader) {
+    }
+
+    /* TODO:
+If connected, sends a QUIT to the server, and closes any established connection
+and then exits the program. This command is valid at any time.
+
+FTP command: QUIT
+Application command: quit
+*/
+    private static void quit(PrintWriter out, BufferedReader reader) {
+    }
+
+    /* TODO:
+Sends the PASSWORD to the FTP server. For an anonymous server the user would typically enter an email
+address or anonymous password command must be sent.
+This typically the second command the user will enter.
+
+FTP command to server: PASS
+Application command: pw, PASSWORD
+*/
+    private static void enterPassword(String[] userInputArray, PrintWriter out, BufferedReader reader) {
+    }
+
+    /*TODO:
+Sends the username to the FTP server. The user will need to pay attention to the response code
+to determine if the password command must be sent.
+This typically the first command the user will enter.
+
+FTP command to Server: USER, PASS
+Application command: user, USERNAME
+*/
+    private static void logIn(String[] userInputArray, PrintWriter out, BufferedReader reader) {
+        out.println(userInputArray[0]);
         try {
-            for (int len = 1; len > 0; ) {
-                System.out.print("csftp> ");
-                len = System.in.read(cmdString);
-
-                /*TODO:
-                Sends the username to the FTP server. The user will need to pay attention to the response code
-                to determine if the password command must be sent.
-                This typically the first command the user will enter.
-
-                FTP command to Server: USER, PASS
-                Application command: user, USERNAME
-                */
-
-                /* TODO:
-                Sends the PASSWORD to the FTP server. For an anonymous server the user would typically enter an email
-                address or anonymous password command must be sent.
-                This typically the second command the user will enter.
-
-                FTP command to server: PASS
-                Application command: pw, PASSWORD
-                */
-
-                /* TODO:
-                If connected, sends a QUIT to the server, and closes any established connection
-                and then exits the program. This command is valid at any time.
-
-                FTP command: QUIT
-                Application command: quit
-                */
-
-                /* TODO:
-                Establishes a data connection and retrieves the file indicated by REMOTE,
-                saving it in a file of the same name on the local machine.
-
-                FTP command: PASV, RETR
-                Application command: get REMOTE
-                */
-
-                /* TODO:
-                Changes the current working directory on the server to the directory indicated by DIRECTORY.
-
-                FTP command: CWD
-                Application Command: cd DIRECTORY
-                */
-
-                /* TODO:
-                Establishes a data connection and retrieves a list of files in the current working directory
-                on the server. The list is printed to standard output.
-
-                FTP command: PASV, LIST
-                Application command: dir
-                */
-
-
-                if (len <= 0)
-                    break;
-                // Start processing the command here.
-                System.out.println("900 Invalid command.");
-            }
-        } catch (IOException exception) {
-            System.err.println("998 Input error while reading commands, terminating.");
+            System.out.println("--> " + reader.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
