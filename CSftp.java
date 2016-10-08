5922c9519395db32a410e63f74c828c712f5b15f
Jongrin Kim <jongrinkim89@gmail.com>
2016-10-08 11:27:26 -0700

    delayed message fixed: solution was to pass in the response once the socket is connected

--- CSftp.java
+++ CSftp.java
@@ -2,6 +2,7 @@
 import java.io.*;
 import java.lang.System;
 import java.net.Socket;
+import java.nio.charset.Charset;
 import java.util.regex.Pattern;
 
 //
@@ -11,15 +12,27 @@ import java.util.regex.Pattern;
 
 
 public class CSftp {
-    static final int MAX_LEN = 255;
-    static final int ARG_CNT = 2;
+    private static final int MAX_LEN = 255;
+    private static final int ARG_CNT = 2;
+    private static String temp;
+    private static String[] userInputArray;
+    private static String[] updatedUserInputArray;
+    private static String response;
+
+    private static void clearByteArray(byte[] cmdString) {
+        for (int i = 0; i < cmdString.length; i++) {
+            cmdString[i] = 0;
+        }
+    }
 
     public static void main(String[] args) {
         byte cmdString[] = new byte[MAX_LEN];
-        PrintWriter out;
-        BufferedReader reader;
-        BufferedReader stdIn;
-        Socket ftpSocket;
+//        PrintWriter out;
+//        BufferedReader reader;
+//        BufferedReader stdIn;
+//        Socket ftpSocket;
+
+
 
         // Get command line arguments and connected to FTP
         // If the arguments are invalid or there aren't enough of them
@@ -36,79 +49,86 @@ public class CSftp {
         if (Pattern.matches(ipFormat, args[0]) || Pattern.matches(DNSFormat, args[0])) {
             String hostname = args[0];
             int portNumber = (args[1] == null)? 21 : Integer.parseInt(args[1]);
-            try {
-                //TODO: Check for timeout
-                //Connect to the ftp using hostname, port number
-                ftpSocket = new Socket(hostname, portNumber);
+            try (
+                    //TODO: Check for timeout
+                    //Connect to the ftp using hostname, port number
+                    Socket ftpSocket = new Socket(hostname, portNumber);
 
-                //Get the socket output stream
-                out = new PrintWriter(ftpSocket.getOutputStream(),true);
-                //get the socket input stream
-                reader = new BufferedReader(new InputStreamReader(ftpSocket.getInputStream()));
-                // To send data to socket
-                stdIn = new BufferedReader(new InputStreamReader(System.in));
+                    //Get the socket output stream
+                    PrintWriter out = new PrintWriter(ftpSocket.getOutputStream(),true);
+                    //get the socket input stream
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(ftpSocket.getInputStream()));
+                    // To send data to socket
+//                stdIn = new BufferedReader(new InputStreamReader(System.in));
+
+            ){
+                while ((response = reader.readLine()) != null) {
+                    System.out.print("<-- " + response + "\n");
+                    if(response.startsWith("2")) {
+                        break;
+                    } else if (response.startsWith("4")) {
+                        System.err.println("920 Control connection to " + hostname + " on port " + portNumber + " failed to open.");
+                    }
+                }
 
                 String userName;
                 String password;
                 //TODO: Need a check for logged into FTP before doing other commands
 
                 for (int len = 1; len > 0; ) {
-                    if(ftpSocket == null) {
-                        ftpSocket = new Socket(hostname, portNumber);
-
-                        //Get the socket output stream
-                        out = new PrintWriter(ftpSocket.getOutputStream(),true);
-                        //get the socket input stream
-                        reader = new BufferedReader(new InputStreamReader(ftpSocket.getInputStream()));
-                        // To send data to socket
-                        stdIn = new BufferedReader(new InputStreamReader(System.in));
-                    }
 
                     System.out.print("csftp> ");
+
+                    //listens to the length of the user input string
                     len = System.in.read(cmdString);
-                    String userInput = new String(cmdString);
-                    String[] userInputArray;
-                    if (userInput.length() != 0) {
-                        userInputArray = userInput.split(" ");
+                    temp = new String(cmdString, Charset.forName("UTF-8"));
 
-                        switch (userInputArray[0].toLowerCase()) {
-                            case "user" :
-                                // Login as user, pass user input array for args
-                                logIn(userInputArray, out, reader, ftpSocket);
-                                cmdString = new byte[MAX_LEN];
-                                break;
+                    //split on 1 or many white spaces
+                    userInputArray = temp.split("\\s+");
+                    //userInputArray gives empty string at the end, so make a copy of the array
+                    // with last element in the array(which is an empty string) removed
+                    updatedUserInputArray = new String[userInputArray.length - 1];
+                    System.arraycopy(userInputArray, 0, updatedUserInputArray, 0, userInputArray.length - 1);
 
-                            case "pw" :
-                                enterPassword(userInputArray, out, reader, ftpSocket);
-                                cmdString = new byte[MAX_LEN];
-                                break;
+                    switch (updatedUserInputArray[0].toLowerCase()) {
+                        case "user" :
+                            // Login as user, pass user input array for args
+                            logIn(updatedUserInputArray, out, reader, ftpSocket);
+                            clearByteArray(cmdString);
+                            break;
 
-                            case "quit" :
-                                quit(out, reader);
-                                break;
+                        case "pw" :
+                            enterPassword(updatedUserInputArray, out, reader, ftpSocket);
+                            clearByteArray(cmdString);
+                            break;
 
-                            case "get" :
-                                getRemote(userInputArray, out, reader);
-                                break;
+                        case "quit" :
+                            quit(out, reader);
+                            System.exit(0);
+                            break;
 
-                            case "cd" :
-                                cdDirectory(userInputArray, out, reader);
-                                break;
+                        case "get" :
+                            getRemote(updatedUserInputArray, out, reader);
+                            break;
 
-                            case "dir" :
-                                showDir(out, reader);
-                                break;
+                        case "cd" :
+                            cdDirectory(updatedUserInputArray, out, reader);
+                            break;
 
-                            case "":
-                                System.out.println("\n");
-                                break;
+                        case "dir" :
+                            showDir(out, reader);
+                            break;
 
-                            default:
-                                // Start processing the command here.
-                                System.out.println("900 Invalid command.");
-                        }
+                        case "":
+                            System.out.println("\n");
+                            break;
 
+                        default:
+                            // Start processing the command here.
+                            System.out.println("900 Invalid command.");
                     }
+
+//                    }
                     if (len <= 0)
                         break;
 
@@ -131,31 +151,6 @@ FTP command: PASV, LIST
 Application command: dir
 */
     private static void showDir(PrintWriter out, BufferedReader reader) {
-        out.println("PASV");
-
-        try {
-            String response = reader.readLine();
-            System.out.println("--> " + response);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        // response includes the port number the client is to establish a data connection on
-        // Todo: create a new socket to use the same server + different port to establish the data connection
-        //Once the client establishes the data connection the file is transferred.
-        // If the ftp client tries to make a data connection to the server and the socket can't be
-        // created or a connection isn't established within a reasonable time (say 30 seconds)
-        // then the attempt to establish the connection should be abandoned, sockets etc closed/destroyed,
-        // the 930 message printed.
-
-
-        out.println("LIST");
-
-        try {
-            String response = reader.readLine();
-            System.out.println("--> " + response);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
 
     }
@@ -196,16 +191,6 @@ FTP command: QUIT
 Application command: quit
 */
     private static void quit(PrintWriter out, BufferedReader reader) {
-        System.out.print("--> QUIT" + "\n");
-        out.println("QUIT");
-
-        try {
-            String response = reader.readLine();
-            System.out.print(response + "\n");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
     }
 
     /* TODO:
@@ -217,26 +202,24 @@ FTP command to server: PASS
 Application command: pw
 */
     private static void enterPassword(String[] userInputArray, PrintWriter out, BufferedReader reader, Socket ftpSocket) {
-        System.out.print("--> PASS " + userInputArray[1]);
+        System.out.print("--> PASS " + userInputArray[1] + "\n");
         out.println("PASS " + userInputArray[1]);
 
         try {
-            if (reader.readLine() != null) {
-                String response = reader.readLine();
+            String response = reader.readLine();
+            
+            String responseCode = response.substring(0, 3);
 
+            //Todo:
+            // processResponse(String responseCode) to handle all the server response
+            // Output: Boolean
+            //         if any one of the bad ones => print the error statements inside this method e.g. 900 , 901, and so on
+            //         if any one of the good ones => you return true or something
+            //      and then we can just System.out.println("<-- " + response) outside
+            //
+            System.out.println("<-- " + response);
+//                reader.close();
 
-                String responseCode = response.substring(0, 3);
-
-                //Todo:
-                // processResponse(String responseCode) to handle all the server response
-                // Output: Boolean
-                //         if any one of the bad ones => print the error statements inside this method e.g. 900 , 901, and so on
-                //         if any one of the good ones => you return true or something
-                //      and then we can just System.out.println("<-- " + response) outside
-                //
-                System.out.println("<-- " + response);
-//                reader.close()
-            }
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -254,27 +237,31 @@ Application command: user, USERNAME
     private static void logIn(String[] userInputArray, PrintWriter out, BufferedReader reader, Socket ftpSocket) {
 
         if(userInputArray.length != 2) {
-            System.out.print("invalid username");
+            System.out.print("invalid username" + "\n");
         } else {
-            System.out.print("--> USER " + userInputArray[1]);
+            System.out.print("--> USER " + userInputArray[1] + "\n");
             out.println("USER " + userInputArray[1]);
 
             try {
-                if (reader.readLine() != null) {
-                    String response = reader.readLine();
 
-                    String responseCode = response.substring(0, 3);
+                String response = reader.readLine();
 
-                    //Todo:
-                    // processResponse(String responseCode) to handle all the server response
-                    // Output: Boolean
-                    //         if any one of the bad ones => print the error statements inside this method e.g. 900 , 901, and so on
-                    //         if any one of the good ones => you return true or something
-                    //      and then we can just System.out.println("<-- " + response) outside
-                    //
+                String responseCode = response.substring(0,3);
 
-                    System.out.println("<-- " + response);
-                }
+                //Todo:
+                // processResponse(String responseCode) to handle all the server response
+                // Output: Boolean
+                //         if any one of the bad ones => print the error statements inside this method e.g. 900 , 901, and so on
+                //         if any one of the good ones => you return true or something
+                //      and then we can just System.out.println("<-- " + response) outside
+                //
+
+                System.out.println("<-- " + response);
+
+//                    reader.close();
+
+
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -283,14 +270,18 @@ Application command: user, USERNAME
 
     }
 
-//    private static boolean processResponse(String responseCode) {
+//    private static boolean processResponse(String responseCode, String hostName, String portNumber) {
 //
 //        switch (responseCode) {
 //            case "331" :
 //                System.out.print("User name okay, need password.");
 //                break;
 //
-//            case "332" :
+//
+//            //When an attempt to establish the connection can't be completed within a reasonable time (say 30 seconds),
+//            // or the socket cannot be created, then print this message, replacing xxx and yyy with the hostname and port number of the target ftp server you are trying to establish the control connection to,.
+//            case "425" :
+//                System.out.print("920 Control connection to " + hostName + " on port " + portNumber + " failed to open");
 //                break;
 //
 //            case "333" :
